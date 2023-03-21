478335ea49e9187750d81b40f161295b24df14c6
raysofgoodness <shady5324@gmail.com>
2023-03-21 13:43:28 +0200

    transfer vclid script

--- assets/vclid-cart.js
+++ assets/vclid-cart.js
@@ -5,13 +5,5 @@ const vclid = urlParams.get('vclid');
 localStorage.setItem('vclid', vclid);
 
 document.addEventListener('DOMContentLoaded', function() {
-    const orderNotesInput = document.querySelector('#note-vclid');
-    const submitButton = document.querySelector('#checkout');
-
-    submitButton.addEventListener('click', function() {
-        const vclid = localStorage.getItem('vclid');
-        if (vclid) {
-            orderNotesInput.value += `VCLID: ${vclid}\n`;
-        }
-    });
+    const vclid = localStorage.getItem('vclid');
 });
