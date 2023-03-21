3f6e538359a1ac1ad4ac3cd32e0c608dbddaf61b
raysofgoodness <shady5324@gmail.com>
2023-03-21 13:46:26 +0200

    added note-vclid input

--- assets/vclid-cart.js
+++ assets/vclid-cart.js
@@ -5,5 +5,10 @@ const vclid = urlParams.get('vclid');
 localStorage.setItem('vclid', vclid);
 
 document.addEventListener('DOMContentLoaded', function() {
+    const orderNotesInput = document.querySelector('#note-vclid');
     const vclid = localStorage.getItem('vclid');
+
+    if (vclid) {
+        orderNotesInput.value += `VCLID: ${vclid}\n`;
+    }
 });
