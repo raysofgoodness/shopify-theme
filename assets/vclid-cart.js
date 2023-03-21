d89db81330aac08073d2c4b245f0d35d034321ee
raysofgoodness <shady5324@gmail.com>
2023-03-21 14:23:35 +0200

    added sessionStorage

--- assets/vclid-cart.js
+++ assets/vclid-cart.js
@@ -1,14 +1,19 @@
-
 const urlParams = new URLSearchParams(window.location.search);
 const vclid = urlParams.get('vclid');
 
-localStorage.setItem('vclid', vclid);
+sessionStorage.setItem('vclid', vclid);
+
+window.addEventListener('beforeunload', function() {
+    const vclid = sessionStorage.getItem('vclid');
+    sessionStorage.setItem('vclid', vclid);
+});
 
 document.addEventListener('DOMContentLoaded', function() {
     const orderNotesInput = document.querySelector('#Cart-note');
-    const vclid = localStorage.getItem('vclid');
+    const vclid = sessionStorage.getItem('vclid');
 
     if (vclid) {
         orderNotesInput.value += `VCLID: ${vclid}\n`;
     }
 });
+
