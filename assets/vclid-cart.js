0619c9f67fa4e804f2aa031d7e57787a788dce11
raysofgoodness <shady5324@gmail.com>
2023-03-21 14:18:08 +0200

    added load window vclid

--- assets/vclid-cart.js
+++ assets/vclid-cart.js
@@ -1,3 +1,4 @@
+
 const urlParams = new URLSearchParams(window.location.search);
 const vclid = urlParams.get('vclid');
 
@@ -5,33 +6,9 @@ localStorage.setItem('vclid', vclid);
 
 document.addEventListener('DOMContentLoaded', function() {
     const orderNotesInput = document.querySelector('#Cart-note');
-    let savedNote = localStorage.getItem('note');
     const vclid = localStorage.getItem('vclid');
 
     if (vclid) {
-        savedNote = savedNote ? `${savedNote}\nVCLID: ${vclid}` : `VCLID: ${vclid}`;
+        orderNotesInput.value += `VCLID: ${vclid}\n`;
     }
-
-    orderNotesInput.value = savedNote;
-
-    orderNotesInput.addEventListener('input', function() {
-        localStorage.setItem('note', orderNotesInput.value);
-    });
 });
-
-window.addEventListener('load', function() {
-    const orderNotesInput = document.querySelector('#Cart-note');
-    let savedNote = localStorage.getItem('note');
-    const vclid = localStorage.getItem('vclid');
-
-    if (vclid) {
-        savedNote = savedNote ? `${savedNote}\nVCLID: ${vclid}` : `VCLID: ${vclid}`;
-    }
-
-    orderNotesInput.value = savedNote;
-
-    orderNotesInput.addEventListener('input', function() {
-        localStorage.setItem('note', orderNotesInput.value);
-    });
-});
-
