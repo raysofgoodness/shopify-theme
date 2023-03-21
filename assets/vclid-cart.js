2bbdfe56dc17541c988c66c40ca428a6bb3829d3
raysofgoodness <shady5324@gmail.com>
2023-03-21 14:25:25 +0200

    replace for the localstorage

--- assets/vclid-cart.js
+++ assets/vclid-cart.js
@@ -1,16 +1,16 @@
 const urlParams = new URLSearchParams(window.location.search);
 const vclid = urlParams.get('vclid');
 
-sessionStorage.setItem('vclid', vclid);
+localStorage.setItem('vclid', vclid);
 
 window.addEventListener('beforeunload', function() {
-    const vclid = sessionStorage.getItem('vclid');
+    const vclid = localStorage.getItem('vclid');
     sessionStorage.setItem('vclid', vclid);
 });
 
 document.addEventListener('DOMContentLoaded', function() {
     const orderNotesInput = document.querySelector('#Cart-note');
-    const vclid = sessionStorage.getItem('vclid');
+    const vclid = localStorage.getItem('vclid');
 
     if (vclid) {
         orderNotesInput.value += `VCLID: ${vclid}\n`;
