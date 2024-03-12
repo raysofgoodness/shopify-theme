b073b99ba818127a2a4090be34767b48cfddcdc9
raysofgoodness <shady5324@gmail.com>
2024-03-12 12:31:56 +0200

    remove capture vclid

--- assets/capture-vclid.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const vclid = urlParams.get('vclid');
-
-    localStorage.setItem('vclid', vclid);
-});
