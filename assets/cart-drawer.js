31a6b448076ee29c0d4eda0cdfcc3abfdd51cfb3
raysofgoodness <shady5324@gmail.com>
2024-03-12 15:22:50 +0200

    styled component card, added logic for form add to cart

--- assets/cart-drawer.js
+++ assets/cart-drawer.js
@@ -1,13 +1,31 @@
-document.querySelectorAll('[data-card-add]').forEach(form => {
-   form.addEventListener('submit', async (e) => {
-      e.preventDefault();
+const addToCartForms = document.querySelectorAll('form[action="/cart/add"]');
 
-       await fetch('/cart/add', {
-           method: "post",
-           body: new FormData(form)
-       });
+addToCartForms.forEach(form => {
+    form.addEventListener('submit', async (e) => {
+        e.stopImmediatePropagation();
+        e.preventDefault();
 
-       document.querySelector('.cart-drawer').classList.add('cart-drawer--active');
+        await fetch('/cart/add', {
+            method: 'post',
+            body: new FormData(form),
+        });
 
-   });
+        const res = await fetch('/cart.json');
+        const cart = await res.json();
+
+        console.log(cart);
+
+        const cartCountBubble = document.querySelector('.cart-count-bubble');
+        if (cartCountBubble) {
+            if (cart.item_count < 100) {
+                cartCountBubble.innerHTML = `<span aria-hidden="true">${cart.item_count}</span><span class="visually-hidden">{{ 'sections.header.cart_count' | t: count: cart.item_count }}</span>`;
+            }
+        } else {
+            const cartIconBubble = document.querySelector('#cart-icon-bubble');
+            if (cartIconBubble) {
+                cartIconBubble.innerHTML += `<div class="cart-count-bubble"><span aria-hidden="true">${cart.item_count}</span><span class="visually-hidden">{{ 'sections.header.cart_count' | t: count: cart.item_count }}</span></div>`;
+            }
+        }
+    });
 });
+
